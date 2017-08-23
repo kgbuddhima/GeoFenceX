89c333cadf2acd4befe6a1e18ef382188f07722f
DESKTOP-LBHS619\Buddhima <kgbuddhima@gmail.com>
2017-08-23 23:16:16 +0530

    PCL : Models -> Constants class added

---
+++ GeoFenceXMobile/GeoFenceX/GeoFenceX/Models/Constants.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoFenceX.Models
+{
+    public static class Constants
+    {
+        // URL of REST service
+        public static string urlGetLocationList = "";
+        public static string urlUpdateAttendence = "";
+        // Credentials that are hard coded into the REST service
+        public static string Username = "Xamarin";
+        public static string Password = "Pa$$w0rd";
+
+        public static string S_Exists = "EXIST";
+        public static string S_OK = "OK";
+        public static string S_NotSuccess = "NOT_SUCCESS";
+    }
+}
