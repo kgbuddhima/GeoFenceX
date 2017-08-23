9905ac5bbe01880d8d5fcaa3c371bc17382a50f3
DESKTOP-LBHS619\Buddhima <kgbuddhima@gmail.com>
2017-08-23 23:17:26 +0530

    PCL _ WebServices -> GeoService, IGeoService classes added

---
+++ GeoFenceXMobile/GeoFenceX/GeoFenceX/WebServices/GeoService.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeoFenceX.Models;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace GeoFenceX.WebServices
+{
+    public class GeoService:IGeoService
+    {
+        HttpClient client;
+
+        public GeoService()
+        {
+            client = new HttpClient();
+            client.MaxResponseContentBufferSize = 256000;
+        }
+
+        /// <summary>
+        /// Get Geo Location List Async from API
+        /// </summary>
+        /// <returns></returns>
+        public async Task<List<Place>> GetGeoLocationsAsync()
+        {
+            List<Place> col = new List<Place>();
+            try
+            {
+                string strContent = "GetRegionList";
+                var json = JsonConvert.SerializeObject(strContent);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = null;
+                response = await client.PostAsync(Constants.urlUpdateAttendence, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    // result = Constants.S_OK;
+                    col = JsonConvert.DeserializeObject<List<Place>>(Convert.ToString(response.Content));
+                }
+                else
+                {
+                   // result = Constants.S_NotSuccess;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return col;
+        }
+
+        /// <summary>
+        /// Update user's attendence data by sending them to API
+        /// </summary>
+        /// <param name="attendenceData"></param>
+        /// <returns></returns>
+        public async Task<string> UpdateAttendence(AttendenceData attendenceData)
+        {
+            string result = Constants.S_NotSuccess;
+            try
+            {
+                
+                var json = JsonConvert.SerializeObject(attendenceData);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = null;
+                response = await client.PostAsync(Constants.urlUpdateAttendence, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    result = Constants.S_OK;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return result;
+        }
+
+    }
+}
---
+++ GeoFenceXMobile/GeoFenceX/GeoFenceX/WebServices/IGeoService.cs
@@ -0,0 +1,25 @@
+﻿using GeoFenceX.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoFenceX.WebServices
+{
+    public interface IGeoService
+    {
+        /// <summary>
+        /// Get Geo Location List Async
+        /// </summary>
+        /// <returns></returns>
+        Task<List<Place>> GetGeoLocationsAsync();
+
+        /// <summary>
+        /// Update user's attendence data by sending them to API
+        /// </summary>
+        /// <param name="attendenceData"></param>
+        /// <returns></returns>
+        Task<string> UpdateAttendence(AttendenceData attendenceData);
+    }
+}
