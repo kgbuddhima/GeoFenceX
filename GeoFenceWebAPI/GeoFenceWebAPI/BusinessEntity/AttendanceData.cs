0ce31bf7ba4008fce0a66277771067a6e378f000
DESKTOP-LBHS619\Buddhima <kgbuddhima@gmail.com>
2017-08-24 16:31:01 +0530

    Web : Business Entity classes added

---
+++ GeoFenceWebAPI/GeoFenceWebAPI/BusinessEntity/AttendanceData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeoFenceWebAPI.BusinessEntity
+{
+    public class AttendanceData : Region
+    {
+        public int RecordId { get; set; }
+
+        public int UserId { get; set; }
+
+        public string TransitionName { get; set; }
+
+        public DateTime TransitionTime { get; set; }
+
+    }
+}
---
+++ GeoFenceWebAPI/GeoFenceWebAPI/BusinessEntity/Region.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeoFenceWebAPI.BusinessEntity
+{
+    public class Region
+    {
+        public string Name { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public double Radius { get; set; }
+    }
+}
